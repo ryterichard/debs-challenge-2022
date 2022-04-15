cf976207c979c294b0c5d4e013223940ffa92f9a
xav-ie <xruizify@gmail.com>
2022-04-15 17:57:40 -0400

    add table

--- frontend/app/src/components/Main.js
+++ frontend/app/src/components/Main.js
@@ -4,7 +4,107 @@ const Main = ()=>{
   return (
 	  <main>
 	  <div className="tableWrapper disable-scrollbars">
-	  <Table/>
+<table>
+	  <thead>
+            <tr>
+              <th><div>Stock</div></th>
+              <th><div>LTP</div></th>
+              <th><div>Action</div></th>
+          </tr>
+	  </thead>
+	  <tbody>
+            <tr>
+              <td>Apple</td>
+              <td>100.00</td>
+              <td><span className="buy">Buy</span></td>
+            </tr>
+            <tr>
+              <td>Google</td>
+              <td>20.00</td>
+              <td><span className="sell">Sell</span></td>
+            </tr>
+            <tr>
+              <td>Tesla</td>
+              <td>00.02</td>
+              <td><span className="hold">Hold</span></td>
+            </tr>
+            <tr>
+              <td>Apple</td>
+              <td>100.00</td>
+              <td><span className="buy">Buy</span></td>
+            </tr>
+            <tr>
+              <td>Google</td>
+              <td>20.00</td>
+              <td><span className="sell">Sell</span></td>
+            </tr>
+            <tr>
+              <td>Tesla</td>
+              <td>00.02</td>
+              <td><span className="hold">Hold</span></td>
+            </tr>
+            <tr>
+              <td>Apple</td>
+              <td>100.00</td>
+              <td><span className="buy">Buy</span></td>
+            </tr>
+            <tr>
+              <td>Google</td>
+              <td>20.00</td>
+              <td><span className="sell">Sell</span></td>
+            </tr>
+            <tr>
+              <td>Tesla</td>
+              <td>00.02</td>
+              <td><span className="hold">Hold</span></td>
+            </tr>
+            <tr>
+              <td>Apple</td>
+              <td>100.00</td>
+              <td><span className="buy">Buy</span></td>
+            </tr>
+            <tr>
+              <td>Google</td>
+              <td>20.00</td>
+              <td><span className="sell">Sell</span></td>
+            </tr>
+            <tr>
+              <td>Tesla</td>
+              <td>00.02</td>
+              <td><span className="hold">Hold</span></td>
+            </tr>
+            <tr>
+              <td>Apple</td>
+              <td>100.00</td>
+              <td><span className="buy">Buy</span></td>
+            </tr>
+            <tr>
+              <td>Google</td>
+              <td>20.00</td>
+              <td><span className="sell">Sell</span></td>
+            </tr>
+            <tr>
+              <td>Tesla</td>
+              <td>00.02</td>
+              <td><span className="hold">Hold</span></td>
+            </tr>
+            <tr>
+              <td>Apple</td>
+              <td>100.00</td>
+              <td><span className="buy">Buy</span></td>
+            </tr>
+            <tr>
+              <td>Google</td>
+              <td>20.00</td>
+              <td><span className="sell">Sell</span></td>
+            </tr>
+            <tr>
+              <td>Tesla</td>
+              <td>00.02</td>
+              <td><span className="hold">Hold</span></td>
+            </tr>
+	  </tbody>
+          </table>
 	  </div>
 	  <div className="graph"></div>
     </main>
