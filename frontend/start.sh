21dc3eb51e41c87d7b8fcdf8631759114b10436f
xav-ie <xruizify@gmail.com>
2022-04-15 17:54:40 -0400

    base frontend

--- frontend/start.sh
+++ frontend/start.sh
@@ -1,6 +1,9 @@
 export KAFKA_HOSTNAME=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1)
 docker rm -vf $(docker-compose ps -a -q)
 docker compose up -d
-cd ./server/
+cd ./app/
+npm ci
+npm run build
+cd ../server/
 npm ci
 npm run server
